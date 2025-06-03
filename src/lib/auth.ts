bbbca4c060351daaca1ba103a937bc18aa6c5862
Nathami <georgenathan010@gmail.com>
2025-06-03 23:54:25 +0300

    add: the route

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,12 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
+  socialProviders: {
+    google: {
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }
+  },
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
